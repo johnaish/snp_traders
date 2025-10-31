327b7f6319837c920643a6898156dfecaa0f6d50
johnaish <jmaish819@gmail.com>
2025-10-31 13:20:34 +0300

    Add temporary superuser creation route

--- snp_traders/urls.py
+++ snp_traders/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "Admin123!")
+        return HttpResponse("✅ Superuser created: admin / Admin123!")
+    return HttpResponse("⚠️ Admin user already exists")
 
 urlpatterns = [
+    path('create-admin/', create_admin),
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
 ]
